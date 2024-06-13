5824ab1de4f9b1deeb80eae9965623fb8bba14dc
Jayesh Kumar <101009853+JayeshK14@users.noreply.github.com>
2024-06-13 20:14:44 +0530

    Update db.js
    Hiding MongoDB username and password.

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try{
         const conn = await mongoose.connect(
-            "mongodb+srv://Jayesh141003:Jayesh141003@cluster0.leejhla.mongodb.net/?retryWrites=true&w=majority&appName=TestDatabase"
+            "mongodb+srv://<username>:<password>@cluster0.leejhla.mongodb.net/?retryWrites=true&w=majority&appName=TestDatabase"
         );
         console.log(`MongoDB connected: ${conn.connection.host}`);
     }
